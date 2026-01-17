149b86c3fa92ec6814c26747cab71ebcf8755dc7
Guilherme Portela <guilhermevnp11@gmail.com>
2026-01-17 19:25:50 -0300

    Add Vercel Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import LandingPage from './components/LandingPage'
+import { Analytics } from '@vercel/analytics/react'
 
 function App() {
-  return <LandingPage />
+  return (
+    <>
+      <LandingPage />
+      <Analytics />
+    </>
+  )
 }
 
 export default App
